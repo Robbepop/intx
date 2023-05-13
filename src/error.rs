0200e450628a349cd3284902fc545489b88efae7
Robin Freyler <robin.freyler@gmail.com>
2023-05-13 12:31:48 +0200

    add docs

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,9 @@
+/// Error that may occur for fallible conversions between integers.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct TryFromIntError(pub(crate) ());
 
 impl From<core::num::TryFromIntError> for TryFromIntError {
+    #[inline]
     fn from(_: core::num::TryFromIntError) -> Self {
         Self(())
     }
